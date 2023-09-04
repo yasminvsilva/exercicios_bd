5846fb35d7d4f2d2c901aa84a115377739e1c6fe
Yasmin <alunoyasminsilva07@gmail.com>
2023-09-03 21:35:30 -0300

    exercício 15

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -74,3 +74,8 @@ LEFT JOIN livros l ON a.id = l.autor_id;
 SELECT a.nome, m.curso
 FROM alunos a
 RIGHT JOIN matriculas m ON a.id = m.aluno_id;
+
+-- 15
+SELECT a.nome, m.curso
+FROM alunos a
+INNER JOIN matriculas m ON a.id = m.aluno_id;
