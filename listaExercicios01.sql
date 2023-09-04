302fbdaa661336de687d1e827ad98784444441dc
Yasmin <alunoyasminsilva07@gmail.com>
2023-09-03 21:24:08 -0300

    exercício 4

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -13,3 +13,9 @@ SELECT l.titulo, a.nome
 FROM livros l
 INNER JOIN autores a ON l.autor_id = a.id
 WHERE nome = 'J.K. Rowling';
+
+-- 4
+SELECT a.nome, m.curso
+FROM alunos a
+INNER JOIN matriculas m ON a.id = m.aluno_id
+WHERE curso = 'Engenharia de Software';
