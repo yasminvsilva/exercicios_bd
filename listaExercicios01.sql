ccfd1e11a575e808ca3829e32bc9806cfe2c3a87
Yasmin <alunoyasminsilva07@gmail.com>
2023-09-03 21:23:17 -0300

    exercício 3

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -7,3 +7,9 @@ SELECT titulo FROM livros;
 SELECT nome
 FROM autores
 WHERE nascimento < '1900-01-01';
+
+-- 3
+SELECT l.titulo, a.nome
+FROM livros l
+INNER JOIN autores a ON l.autor_id = a.id
+WHERE nome = 'J.K. Rowling';
