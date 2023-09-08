c16f22e9589533112bae9f6b72877a4a9d6da029
Yasmin <alunoyasminsilva07@gmail.com>
2023-09-08 13:41:42 -0300

    exercício 20

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -103,3 +103,10 @@ SELECT a.nome, COUNT(aluno_id) AS cursos_matriculados
 FROM alunos a
 INNER JOIN matriculas m ON a.id = m.aluno_id
 GROUP BY a.nome;
+
+-- 20
+SELECT produto, COUNT(*) AS total_vendas
+FROM vendas
+GROUP BY produto
+ORDER BY total_vendas DESC
+LIMIT 1;
