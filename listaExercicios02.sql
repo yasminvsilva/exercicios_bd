2dea82b279e2573bc3ac283f1fc5463d4858cbd0
Yasmin <alunoyasminsilva07@gmail.com>
2023-09-26 22:37:30 -0300

    exercício 2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -11,3 +11,17 @@ END;
 DELIMITER ;
 
 CALL sp_ListarAutores();
+
+-- 2
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN nm_categoria VARCHAR(50))
+BEGIN
+	SELECT l.Titulo, c.Nome AS Categoria
+    FROM Livro l
+    INNER JOIN Categoria c ON l.Categoria_ID = c.Categoria_ID
+    WHERE c.Nome = nm_categoria;
+END;
+//
+DELIMITER ;
+
+CALL sp_LivrosPorCategoria('Autoajuda');
